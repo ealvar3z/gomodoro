ffaf97bf207ab520254209bfd6fc818ad9f31d5e
e. alvarez <55966724+ealvar3z@users.noreply.github.com>
2023-09-15 15:57:34 -0400

    feat: Enhance Pomodoro CLI with Bubble Tea and Lipgloss
    This commit integrates the Bubble Tea architecture and Lipgloss styling
    to improve the user interface and experience of the Pomodoro timer CLI tool.
    
    Changes:
    - Refactor the main application loop to use Bubble Tea's Model-Update-View architecture
    - Add a spinner for visual feedback during different Pomodoro phases
    - Implement key bindings for quitting the application

--- main.go
+++ main.go
@@ -3,57 +3,103 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/signal"
 	"time"
+
+	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/spinner"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
-func timer(d time.Duration) {
-	ticker := time.NewTicker(1 * time.Second)
-	done := make(chan bool)
+type tickMsg time.Duration
 
-	go func() {
-		time.Sleep(d)
-		done <- true
-	}()
+type model struct {
+	spinner   spinner.Model
+	timeLeft  time.Duration
+	totalTime time.Duration
+	mode      string
+	gomos     int
+	quitting  bool
+}
 
-	for {
-		select {
-		case <-done:
-			ticker.Stop()
-			return
-		case t := <-ticker.C:
-			fmt.Printf("\r%s", t.Format("15:04:05"))
-		}
+var quitKeys = key.NewBinding(
+	key.WithKeys("q", "esc", "ctrl+c"),
+	key.WithHelp("", "press q to quit"),
+)
+
+func initialModel() model {
+	s := spinner.New()
+	s.Spinner = spinner.Dot
+	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+
+	return model{
+		spinner:   s,
+		timeLeft:  25 * time.Minute,
+		totalTime: 25 * time.Minute,
+		mode:      "Work",
+		gomos:     0,
 	}
 }
 
-func main() {
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
+func (m model) Init() tea.Cmd {
+	return tea.Tick(time.Second, func(t time.Time) tea.Msg {
+		return tickMsg(1 * time.Second)
+	})
+}
 
-	go func() {
-		<-c
-		fmt.Println("\nExiting...")
-		os.Exit(0)
-	}()
-
-	work := 25 * time.Minute
-	rest := 5 * time.Minute
-	longRest := 15 * time.Minute
-
-	gomos := 0
-	for {
-		fmt.Println("Work!")
-		timer(work)
-		gomos++
-
-		if gomos%4 == 0 {
-			fmt.Println("Long rest!")
-			timer(longRest)
-		} else {
-			fmt.Println("Rest!")
-			timer(rest)
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		if key.Matches(msg, quitKeys) {
+			m.quitting = true
+			return m, tea.Quit
 		}
-	}
+		return m, nil
 
+	case tickMsg:
+		m.timeLeft -= time.Duration(msg)
+		// newPercent := float64(100 - int((m.timeLeft*100)/m.totalTime))
+		if m.timeLeft <= 0 {
+			if m.mode == "Work" {
+				m.gomos++
+				if m.gomos%4 == 0 {
+					m.mode = "Long Rest"
+					m.timeLeft = 15 * time.Minute
+					m.totalTime = 15 * time.Minute
+				} else {
+					m.mode = "Rest"
+					m.timeLeft = 5 * time.Minute
+					m.totalTime = 5 * time.Minute
+				}
+			} else {
+				m.mode = "Work"
+				m.timeLeft = 25 * time.Minute
+				m.totalTime = 25 * time.Minute
+			}
+		}
+		return m, tea.Tick(time.Second, func(t time.Time) tea.Msg {
+			return tickMsg(1 * time.Second)
+		})
+
+	default:
+		var cmd tea.Cmd
+		m.spinner, cmd = m.spinner.Update(msg)
+		return m, cmd
+	}
+}
+
+func (m model) View() string {
+	str := fmt.Sprintf("\n\n   %s %s Time Left: %s %s\n\n", m.spinner.View(), m.mode, m.timeLeft, quitKeys.Help().Desc)
+	if m.quitting {
+		return str + "\n"
+	}
+	return str
+}
+
+func main() {
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
